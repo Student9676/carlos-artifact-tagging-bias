218cba4b858600bfbadab9488624c87d535c4af2
Raasikh Kanjiani <64110149+Student9676@users.noreply.github.com>
2024-07-19 23:04:05 -0400

    updated preprocessing code to handle NaNs

--- bert/preprocess.py
+++ bert/preprocess.py
@@ -32,12 +32,26 @@ def get_processed_data(with_labels):
 	# Remove rows with empty descriptions
 	# annotated_data = annotated_data.dropna(subset=["TextEntry"])
 	
+	# Replace all empty descriptions and titles with empty strings for internal processing (i.e. the step below)
+	annotated_data["TextEntry"] = annotated_data["TextEntry"].fillna("")
+	annotated_data["Title"] = annotated_data["Title"].fillna("")
+
 	# Combine Title and TextEntry columns
 	annotated_data["TextEntry"] = annotated_data["Title"] + "\n\n" + annotated_data["TextEntry"]
 	annotated_data = annotated_data.drop(columns=["Title"])
 
+	# Strip descriptions for clean data
+	for index, row in annotated_data.iterrows():
+		annotated_data.at[index, "TextEntry"] = row["TextEntry"].strip()
+
+	# Fill na's in classification columns with zeros just incase an annotater left a cell empty
+	annotated_data["Subjective"] = annotated_data["Subjective"].fillna("")
+	annotated_data["Gender"] = annotated_data["Gender"].fillna("")
+	annotated_data["Jargon"] = annotated_data["Jargon"].fillna("")
+	annotated_data["Social"] = annotated_data["Social"].fillna("")
+
 	# Remove carriage returns
-	annotated_data = annotated_data.replace('_x000D_', '', regex=True)
+	annotated_data = annotated_data.replace("_x000D_", "", regex=True)
 
 	print(annotated_data.head(20))
 	return annotated_data
@@ -48,4 +62,4 @@ with open("clean_data.py") as cleaner:
 	exec(cleaner.read())
 print("----------\nFINISHED RUNNING clean_data.py\n")
 
-get_processed_data(with_labels=False).to_excel("carlos_data/preprocessed_data.xlsx", engine="xlsxwriter", index=False)
+get_processed_data(with_labels=False).to_excel("carlos_data/preprocessed_data_v1.xlsx", engine="xlsxwriter", index=False)
