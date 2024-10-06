4f7e31f995de91234d5a732360a26a3e956255c9
Raasikh Kanjiani <64110149+Student9676@users.noreply.github.com>
2024-10-06 02:24:36 -0400

    fixed script data input and output for simple files

--- bert/preprocess.py
+++ bert/preprocess.py
@@ -1,91 +1,104 @@
-# Input: clean data >=v2, annotated data >=v2
+# Input: annotated data
 # Output: processed data for bert training called preprocessed_data.xlsx
 
 import pandas as pd
+import regex as re
 
-def get_processed_data(with_labels):
-	# Get annotations only
-	annotations_raw = pd.read_excel("carlos_data/clean_data_annotated_v2_July_18.xlsx")
-	annotations_raw = annotations_raw.filter(["Subjective", "Gender", "Jargon", "Social"])
+# Get annotated data
+annotated_data = pd.read_excel("carlos_data/annotated_data.xlsx")
 
-	# Keep rows 0-600, 900-1000, 1500-1600
-	annotations_clean = annotations_raw.iloc[0:601]
-	annotations_clean = pd.concat([annotations_clean, annotations_raw.iloc[900:1001]])
-	annotations_clean = pd.concat([annotations_clean, annotations_raw.iloc[1500:1601]])
-	# filtered = filtered._append(raw.iloc[900:1001])
-	# filtered = filtered._append(raw.iloc[1500:1601])
+# Replace all empty descriptions and titles with empty strings for internal processing (i.e. the step below)
+annotated_data["TextEntry"] = annotated_data["TextEntry"].fillna("")
+annotated_data["Title"] = annotated_data["Title"].fillna("")
+
+# Combine Title and TextEntry columns
+annotated_data["TextEntry"] = annotated_data["Title"] + "\n\n" + annotated_data["TextEntry"]
+annotated_data = annotated_data.drop(columns=["Title"])
+
+# Now we want to convert the values in the label columns to arrays of phrases/words
+
+# Function to convert strings to list of strings and the characters '""' to NA
+def string_to_array(s):
+	s = str(s)
+	if pd.isna(s) or s == '""' or s == '':
+		return pd.NA
+	return [phrase.strip() for phrase in re.split(r',\s*|\s*,|\n', s) if phrase.strip()]
+
+# Replace empty strings with NaNs and convert strings, to list
+for column in ['Subjective Label', 'Gender Label', 'Jargon Label', 'Social Label']:
+	annotated_data[column] = annotated_data[column].apply(string_to_array)
+
+# Remove carriage returns
+annotated_data = annotated_data.replace("_x000D_", "", regex=True)
+
+# Now we will alter descriptions to only keep sentences where any of the phrases is present,
+# making sure that there is only one sentence per phrase 
+descs = annotated_data['TextEntry'].values
+jargon_labels_array = annotated_data['Jargon Label'].values
+subjective_labels_array = annotated_data['Subjective Label'].values
+social_labels_array = annotated_data['Social Label'].values
+gender_labels_array = annotated_data['Gender Label'].values
+
+# POSSIBLE OPTMIZATION by removing ids like "X.0427.004 B" and "1969.127.029" from the resulting lists
+# Get segmenter
+import pysbd
+seg = pysbd.Segmenter(language="en", clean=False)
+
+# segment each description into sentences
+for i in range(len(descs)):
+	descs[i] = seg.segment(descs[i])
 	
-	# Get clean data w/ artifact info
-	raw = pd.read_excel("carlos_data/clean_data_v2.xlsx")
-	raw = raw.filter(["ObjectID", "Title", "TextEntry"])
+	# strip all sentences
+	for j in range(len(descs[i])):
+		descs[i][j] = descs[i][j].strip()
 
-	# Keep rows 0-600, 900-1000, 1500-1600
-	clean = raw.iloc[0:601]
-	clean = pd.concat([clean, raw.iloc[900:1001]])
-	clean = pd.concat([clean, raw.iloc[1500:1601]])
-	# filtered = filtered._append(raw.iloc[900:1001])
-	# filtered = filtered._append(raw.iloc[1500:1601])
+new_descs = []
 
-	# Join titles and labels with annotations
-	annotated_data_1 = clean.join(annotations_clean)
+# Main loop
+for sentences, subjective_labels, gender_labels, jargon_labels, social_labels in \
+zip(descs, subjective_labels_array, gender_labels_array, jargon_labels_array, social_labels_array):
+	new_desc = ""
 
-	#-------------------------V3 ANNOTATIONS START-------------------------
-
-	annotations_raw_2 = pd.read_excel("carlos_data/clean_data_annotated_shuffled_v3_double_checked.xlsx", index_col=0)
-	annotations_raw_2 = annotations_raw_2.filter(["ObjectID", "Subjective", "Gender", "Jargon", "Social"])
-
-	# Keep rows 0-1000 
-	annotations_clean_2 = annotations_raw_2.loc[0:1001]
-
-	raw = pd.read_excel("carlos_data/clean_data_v2.xlsx")
-	raw = raw.filter(["ObjectID", "Title", "TextEntry"])
-
-	# keep only objects in raw that are present in annotated shuffled data using objectid
-	filtered_raw = raw[raw['ObjectID'].isin(annotations_clean_2['ObjectID'])]
-
-	# Reset index for both dataframes
-	filtered_raw.reset_index(drop=True, inplace=True)
-	annotations_clean_2.reset_index(drop=True, inplace=True)
-
-	# Merge both dataframes on 'ObjectID'
-	annotated_data_2 = pd.merge(filtered_raw, annotations_clean_2, on='ObjectID', how='inner')
+	subjective_labels = [] if not hasattr(subjective_labels, '__len__') and pd.isna(subjective_labels) else subjective_labels
+	gender_labels = [] if not hasattr(gender_labels, '__len__') and pd.isna(gender_labels) else gender_labels
+	jargon_labels = [] if not hasattr(jargon_labels, '__len__') and pd.isna(jargon_labels) else jargon_labels
+	social_labels = [] if not hasattr(social_labels, '__len__') and pd.isna(social_labels) else social_labels
+	phrases = [*subjective_labels, *gender_labels, *jargon_labels, *social_labels]
 	
-	annotated_data = pd.concat([annotated_data_1, annotated_data_2], ignore_index=True)
-
-	#----------------------------V3 ANNOTATIONS END---------------------------------
-
-	# Remove rows with empty descriptions
-	# annotated_data = annotated_data.dropna(subset=["TextEntry"])
+	if len(phrases) == 0:
+		for sentence in sentences:
+			new_desc += sentence + " "
+		new_descs.append(new_desc)
+		continue
 	
-	# Replace all empty descriptions and titles with empty strings for internal processing (i.e. the step below)
-	annotated_data["TextEntry"] = annotated_data["TextEntry"].fillna("")
-	annotated_data["Title"] = annotated_data["Title"].fillna("")
+	for sentence in sentences:
+		for phrase in phrases:
+			if phrase.lower().replace('"', '') in sentence.lower().replace('"', ''):
+				new_desc += sentence + " "
+				# OPTIMIZATION if phrase size == sentence size then include n words surrounding it
+				phrases.remove(phrase)
+	
+	if len(phrases) != 0:
+		for phrase in phrases:
+			new_desc += phrase + ". "
 
-	# Combine Title and TextEntry columns
-	annotated_data["TextEntry"] = annotated_data["Title"] + "\n\n" + annotated_data["TextEntry"]
-	annotated_data = annotated_data.drop(columns=["Title"])
+	new_descs.append(new_desc.strip())
 
-	# Strip descriptions for clean data
-	for index, row in annotated_data.iterrows():
-		annotated_data.at[index, "TextEntry"] = row["TextEntry"].strip()
+annotated_data["TextEntry"] = new_descs
 
-	# Fill na's in classification columns with zeros just incase an annotater left a cell empty
-	annotated_data["Subjective"] = annotated_data["Subjective"].fillna(0)
-	annotated_data["Gender"] = annotated_data["Gender"].fillna(0)
-	annotated_data["Jargon"] = annotated_data["Jargon"].fillna(0)
-	annotated_data["Social"] = annotated_data["Social"].fillna(0)
+# Strip descriptions for clean data
+for index, row in annotated_data.iterrows():
+	annotated_data.at[index, "TextEntry"] = row["TextEntry"].strip()
 
-	# Remove carriage returns
-	annotated_data = annotated_data.replace("_x000D_", "", regex=True)
+# Fill na's in classification columns with zeros just incase an annotater left a cell empty
+annotated_data["Subjective"] = annotated_data["Subjective"].fillna(0)
+annotated_data["Gender"] = annotated_data["Gender"].fillna(0)
+annotated_data["Jargon"] = annotated_data["Jargon"].fillna(0)
+annotated_data["Social"] = annotated_data["Social"].fillna(0)
 
-	print(annotated_data.head(20))
-	print(annotated_data.shape)
-	return annotated_data
+# Remove carriage returns
+annotated_data = annotated_data.replace("_x000D_", "", regex=True)
 
-
-print("RUNNING clean_data.py")
-with open("clean_data.py") as cleaner:
-	exec(cleaner.read())
-print("----------\nFINISHED RUNNING clean_data.py\n")
-
-get_processed_data(with_labels=False).to_excel("carlos_data/preprocessed_data_v3.xlsx", engine="xlsxwriter", index=False)
+print(annotated_data.head(20))
+print(annotated_data.shape)
+annotated_data.to_excel("carlos_data/preprocessed_data.xlsx", engine="xlsxwriter", index=False)
