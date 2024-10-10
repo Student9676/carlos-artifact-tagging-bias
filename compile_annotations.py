4a8acf6b91449d57aaed73e164cc76b5db81d449
Raasikh Kanjiani <64110149+Student9676@users.noreply.github.com>
2024-10-09 22:01:36 -0400

    updated to add new annotated binary column and write default values

--- compile_annotations.py
+++ compile_annotations.py
@@ -15,18 +15,33 @@ all_annotations = pd.concat([all_annotations, v3.iloc[0:1001]], ignore_index=Fal
 all_annotations.reset_index(drop = True)
 
 # Keep rows that are not present in the df of annotations
-not_annotated = clean_data[~clean_data['ObjectID'].isin(all_annotations['ObjectID'])]
+not_annotated = clean_data[~clean_data["ObjectID"].isin(all_annotations["ObjectID"])]
 
 # Concatenate annotated data and the unannotated data
 df_for_annotations = pd.concat([all_annotations, not_annotated], ignore_index=True)
 
 # Remove old index columns and hexadecimal codes from windows machines (idk how these appeared)
 df_for_annotations = df_for_annotations.drop(columns=["Unnamed: 0"])
-df_for_annotations.replace(r'_x[0-9A-Fa-f]{4}_', '', regex=True, inplace=True)
+df_for_annotations.replace(r"_x[0-9A-Fa-f]{4}_", "", regex=True, inplace=True)
 
 print("Saving Data...")
 
 # Sync index column with excel indices
 df_for_annotations.index = range(2, 2 + len(df_for_annotations))
+
+# Columns to change values to 0
+columns_to_zero = ["Subjective", "Gender", "Jargon", "Social"]
+
+# Columns to change values to an empty string
+columns_to_empty_string = ["Subjective Label", "Gender Label", "Jargon Label", "Social Label"]
+
+# Add default values for rows after index 1805
+df_for_annotations.loc[df_for_annotations.index > 1805, columns_to_zero] = 0
+df_for_annotations.loc[df_for_annotations.index > 1805, columns_to_empty_string] = '""'
+
+# Add a column called annotated? and set default values
+df_for_annotations.insert(df_for_annotations.columns.get_loc('Subjective'), 'ANNOTATED?', "", allow_duplicates=False)
+df_for_annotations.loc[df_for_annotations.index <= 1805, 'ANNOTATED?'] = 1
+df_for_annotations.loc[df_for_annotations.index > 1805, 'ANNOTATED?'] = 0
 
 df_for_annotations.to_excel("carlos_data/annotated_data.xlsx", engine="xlsxwriter")
