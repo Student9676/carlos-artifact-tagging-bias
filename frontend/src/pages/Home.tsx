d15ed5411e3c4d59d8a4b9dacaecf0907f340c1c
Raasikh Kanjiani <64110149+Student9676@users.noreply.github.com>
2025-07-06 11:25:07 -0400

    added website header

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,20 @@
 export default function Home() {
   return (
-    <div className="w-screen h-screen flex items-center justify-center bg-black">
-      <h1 className="text-white text-4xl font-bold">Welcome to the Carlos App</h1>
+    <div className="bg-white">
+      <div className="flex w-screen h-24 items-center p-8"> 
+        <a className="justify-left flex space-x-8 w-max items-center" href="/"><img src="https://carlos.emory.edu/themes/custom/zurbcarlos/logo.svg" alt="Emory Michael C. Carlos Museum Logo" className="h-12" /></a>
+        
+        <div className="justify-right space-x-2 ml-auto w-max items-center flex text-[18px]">
+          <a className=" hover:bg-gray-300 p-4 rounded-md transition-colors duration-300" href="/debiaser">Debiaser</a>
+          <a className=" hover:bg-gray-300 p-4 rounded-md transition-colors duration-300" href="/login">Login</a>
+          <a className=" hover:bg-gray-300 p-4 rounded-md transition-colors duration-300" href="/signup">Signup</a>
+        </div>
+      </div>
+
+      <div className="w-screen h-screen flex items-center justify-center bg-black">
+        <h1 className="text-white text-4xl font-bold">Welcome to the Carlos App</h1>
+      </div>
+      <p className="text-gray-700 text-lg">This is the home page.</p>
     </div>
   );
 }
